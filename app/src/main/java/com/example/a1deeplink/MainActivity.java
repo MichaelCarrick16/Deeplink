74da9de062423558ec0ee3e463db778f24003273
HiepPD <hieppdutck59@gmail.com>
2022-07-17 11:00:23 +0700

    first commit

---
+++ app/src/main/java/com/example/a1deeplink/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.a1deeplink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Lấy ra đường dẫn uri mà khi click từ ngoài app
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        // Handle logic
+        String id;
+        if(uri != null){
+            String getID[] = uri.toString().trim().split("=");
+            id = getID[1].trim();
+            if(id.equals("1")){
+                startActivity(new Intent(this,SecondAct.class));
+                finish();
+            }else{
+                Toast.makeText(this, "ID = "+id+" không phải ID = 1", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
